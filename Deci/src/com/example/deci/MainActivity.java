55411f65410d37242be006b4c4bf00a448b8d718
a <a@121.152.238.252>
2015-11-11 18:18:58 +0900

    f

--- Deci/src/com/example/deci/MainActivity.java
+++ Deci/src/com/example/deci/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.view.View; 
 
 
 public class MainActivity extends Activity {
